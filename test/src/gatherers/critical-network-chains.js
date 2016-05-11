4065e12267ebb738ac0b337f61cab748c21dfae1
Deepanjan Roy <dproy@google.com>
2016-05-10 21:56:49 -0400

    Fix tests - real set deep equal

--- test/src/gatherers/critical-network-chains.js
+++ test/src/gatherers/critical-network-chains.js
@@ -15,10 +15,54 @@
  */
 'use strict';
 
+/* eslint-env mocha */
+
 const GathererClass = require('../../../src/gatherers/critical-network-chains');
 const assert = require('assert');
 
 const Gatherer = new GathererClass();
+
+// We'll have to pull a lot of shenanigans becase assert.deepEqual does not
+// properly support ES6 sets yet (see github.com/nodejs/node/issues/2309)
+
+/**
+ * Boolean version of assert.deepEqual
+ * Obviously this doesn't work on ES6 sets
+ */
+function isDeepEqual(obj1, obj2) {
+  try {
+    assert.deepEqual(obj1, obj2);
+    return true;
+  } catch (e) {
+    return false;
+  }
+}
+
+function deepEqualSet(set1, set2) {
+  if (set1.size !== set2.size) {
+    return false;
+  }
+
+  const set2Copy = new Set([...set2.values()]);
+  // Horrible horrible O(n^2) check. I assume the max size of sets will be
+  // 10 and thus hereby declare this O(1).
+  for (let elm of set1.values()) {
+    let found = false;
+    for (let set2elm of set2Copy.values()) {
+      if (isDeepEqual(elm, set2elm)) {
+        found = true;
+        // Tiny optimization
+        set2Copy.delete(set2elm);
+        break;
+      }
+    }
+    if (!found) {
+      return false;
+    }
+  }
+
+  return true;
+}
 
 function mockTracingData(prioritiesList, edges) {
   const networkRecords = prioritiesList.map((priority, index) =>
@@ -26,12 +70,19 @@ function mockTracingData(prioritiesList, edges) {
         initialPriority: () => priority,
         initiatorRequest: () => null}));
 
+  // Add a high priority request at the beginning - that's the first webpage req
+  const firstNetworkRequest = {
+    requestId: '-1',
+    initialPriority: () => VERY_HIGH,
+    initiatorRequest: () => null
+  };
+
   // add mock initiator information
   edges.forEach(edge => {
     networkRecords[edge[1]].initiatorRequest = () => ({requestId: edge[0]});
   });
 
-  return networkRecords;
+  return [firstNetworkRequest].concat(networkRecords);
 }
 
 function testGetCriticalChain(data) {
@@ -41,7 +92,7 @@ function testGetCriticalChain(data) {
   const requestIdChains = criticalChains.map(chain =>
     chain.map(node => node.requestId));
   // Ordering of the chains do not matter
-  assert.deepEqual(new Set(requestIdChains), new Set(data.expectedChains));
+  assert(deepEqualSet(new Set(requestIdChains), new Set(data.expectedChains)));
 }
 
 const HIGH = 'High';
@@ -50,8 +101,28 @@ const MEDIUM = 'Medium';
 const LOW = 'Low';
 const VERY_LOW = 'VeryLow';
 
-/* global describe, it*/
+
 describe('CriticalNetworkChain gatherer: getCriticalChain function', () => {
+  describe('Meta Testing: deepEqualSet', () => {
+    it('says equal sets are equal', () => {
+      const a = new Set([1, 2, 3]);
+      const b = new Set([1, 3, 2]);
+      assert(deepEqualSet(a, b));
+    });
+
+    it('says deeply equal sets are equal', () => {
+      const a = new Set([[0, 1], [1, 2], [2, 3]]);
+      const b = new Set([[0, 1], [2, 3], [1, 2]]);
+      assert(deepEqualSet(a, b));
+    });
+
+    it('says unequal sets are unequal', () => {
+      const a = new Set([1, 2, 3]);
+      const b = new Set([1, 2]);
+      assert(!deepEqualSet(a, b));
+    });
+  });
+
   it('returns correct data for chain of four critical requests', () =>
     testGetCriticalChain({
       priorityList: [HIGH, MEDIUM, VERY_HIGH, HIGH],
