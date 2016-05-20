b9ad910c3acb1d8d2d40b85aa47c798bc76f2619
Deepanjan Roy <dproy@google.com>
2016-05-20 14:27:34 -0400

    typo

--- src/scheduler.js
+++ src/scheduler.js
@@ -97,7 +97,7 @@ function flattenArtifacts(artifacts) {
 
 function saveArtifacts(artifacts) {
   const artifactsFilename = 'artifacts.log';
-  // The _target properly of NetworkRequest is circular.
+  // The _target property of NetworkRequest is circular.
   // We skip it when stringifying.
   const replacer = (key, value) => key === '_target' ? undefined : value;
   fs.writeFileSync(artifactsFilename, JSON.stringify(artifacts, replacer));
